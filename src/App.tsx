1dbf84748a5c1324ccf20ccd706b976e0051bb99
afteryoon <khsso102649@gmail.com>
2024-01-05 11:52:10 +0900

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import Layout from './layout/Layout';
+import Home from './routes/Home';
+import Project from './routes/Project';
+import Resume from './routes/Resume';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route index element={<Home/>}/>
+            <Route path='project' element={<Project/>}/>
+            <Route path='resume' element={<Resume/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
