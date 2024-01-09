313d0fecd61498915a3c0c20ceb915c3898dca63
afteryoon <khsso102649@gmail.com>
2024-01-09 15:50:36 +0900

    수정

--- src/components/resume/Experience.jsx
+++ src/components/resume/Experience.jsx
@@ -1,15 +1,15 @@
-const Experience = () =>{
+const Experience = ({ projectName, proInfo, date, add }) =>{
 
 
     return(
         
 
         <div className="experience">
-            <h2>프로젝트</h2>
-            <h4>프로젝트 이름</h4>
-            <h4>소개</h4>
-            <h4>프로젝트 기간</h4>
-            <h4>추가정보</h4>
+            <h2>Work Experience _학원 수료일</h2>
+            <h4>{company}</h4>
+            <h4>{position}</h4>
+            <h4>{date}</h4>
+            <h4>{responsibilities}</h4>
         </div>
     )
 }
