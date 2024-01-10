a13ca951cf25806d20df74dab4dba2eb227edfa1
ssusss <kdc19970526@gamil.com>
2024-01-10 19:20:20 +0900

    project완성

--- src/routes/Project.jsx
+++ src/routes/Project.jsx
@@ -1,26 +1,34 @@
 import { readConfigFile } from "typescript";
 import Carousel from 'react-bootstrap/Carousel';
-import ExampleCarouselImage from '../components/project/ExampleCarouselImage';
+import BackgroundCarouselImage from '../components/project/BackgroundCarouselImage';
 
 const Project = () => {
 
-    // 배열 형식 => ["프로젝트명", "이미지url","기간", "참여인원", "간단설명", ["기능1", "기능2", "기능3"], ["기술스택1", "기술스택2", "기술스택3"]];ㅇㄹㄴㅇㄹㄴㅇㄹㄴ
-    const myProject1 = ["프로젝트명", "https://via.placeholder.com/300x500", "기간", "참여인원", "간단설명간단설명간단설명간단설명간단설명간단설명간단설명간단설명간단설명간단설명간단설명간단설명", ["기능1", "기능2", "기능3"], ["기술스택1", "기술스택2", "기술스택3"]];
-
+    // 배열 형식 => ["프로젝트명", "이미지url","기간", "참여인원", "간단설명", ["기능1", "기능2", "기능3"], ["기술스택1", "기술스택2", "기술스택3"]];
+    const myProject1 = [
+        "프로젝트명", 
+        ["https://via.placeholder.com/400x500", "https://via.placeholder.com/401x500", "https://via.placeholder.com/402x500", "https://via.placeholder.com/403x500"], 
+        "기간", 
+        "참여인원", 
+        "간단설명간단설명간단설명간단설명간단설명간단설명간단설명간단설명간단설명간단설명간단설명간단설명", 
+        ["기능1", "기능2", "기능3"], 
+        ["기술스택1", "기술스택2", "기술스택3"]
+    ];
+    const myProject1_github = 'https://github.com/newTypeo/dagachi';
 
     return (
         <div className="projectContainer">
-            <Carousel interval={null}>
+            <Carousel interval={null} as={'div'} >
                 <Carousel.Item>
-                    <ExampleCarouselImage projectInfo = {myProject1} />
+                    <BackgroundCarouselImage projectInfo = {myProject1} myProject1_github = {myProject1_github} />
                     
                 </Carousel.Item>
                 <Carousel.Item>
-                    <ExampleCarouselImage projectInfo = {myProject1} />
+                    <BackgroundCarouselImage projectInfo = {myProject1} />
                     
                 </Carousel.Item>
                 <Carousel.Item>
-                    <ExampleCarouselImage projectInfo = {myProject1} />
+                    <BackgroundCarouselImage projectInfo = {myProject1} />
                     
                 </Carousel.Item>
             </Carousel>
