d97cb4c6453e752e74f2c65d6a0069e7bdfc1a4e
afteryoon <khsso102649@gmail.com>
2024-01-16 17:23:53 +0900

    한페이지 ver

---
+++ src/routes/OnePageVer.jsx
@@ -0,0 +1,30 @@
+import IndexMyInfo from "../components/IndexMyInfo";
+import MyselfComponent from "../components/MyselfComponent";
+import Features from "../components/home/Features";
+import Project from "./Project";
+import Resume from "./Resume";
+
+const OnepageVer=()=>{
+
+    return(
+        <div >
+
+            <div className="homeContainer">
+                <IndexMyInfo />
+                <MyselfComponent />
+            </div>
+
+            <div>
+                    <Features />
+            </div>
+
+            <Resume/>
+            <Project/>
+            
+        
+
+        </div>
+    )
+}
+
+export default OnepageVer;
